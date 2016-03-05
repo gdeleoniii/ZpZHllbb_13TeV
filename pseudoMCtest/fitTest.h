510ac5cd51d0149776f3bc02f3cdf0f08986508c
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-03-05 13:57:12 +0100

    separate to many header files

---
+++ pseudoMCtest/fitTest.h
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <TF1.h>
+#include <TH1.h>
+#include <TMatrixD.h>
+#include <TFitResult.h>
+#include <TGraphAsymmErrors.h>
+#include "fitFunction.h"
+#include "fitUncertainty.h"
+
+/*
+  Steps to study fit bias and pull by generating n toy-MCs :
+
+  1. Fefore doing anything, make the stat error of MC more like data.
+  2. Fit pruned mass of (signal + sideband) by only free the shape parameters; using equation A.
+  3. Fit pruned mass of (side band only) by using the shape parameters from (2), only free the amplitude; using equation B.
+  4. Use the fit function from (3) to generate a lot of toy samples (using TF1::GetRandom()).
+  5. Remove the signal region of these toy samples, fit them using equation B, only free the amplitude.
+  6. Calculate bias and pull. For the pull, the denominator is the  uncertainty based on error matrix.
+*/
+
+void fitTest(TH1D* h_prmass, TH1D* h_prmass_hollow,
+	     TGraphAsymmErrors* g_errorBands, TGraphAsymmErrors* g_errorBands_hollow, 
+	     TH1D* h_bias, TH1D* h_upPull, TH1D* h_dwPull){
+  
+  /// Make the statistics error of MC more like data
+
+  for( int i = 1; i <= h_prmass->GetNbinsX(); i++ ){
+
+    h_prmass->SetBinError(i,TMath::Sqrt(h_prmass->GetBinContent(i)));
+    h_prmass_hollow->SetBinError(i,TMath::Sqrt(h_prmass_hollow->GetBinContent(i)));
+
+  }
+
+  /// Fit pruned mass of (signal + sideband) by only free the shape parameters
+
+  TF1* f_fitprmass = new TF1("f_fitprmass", fitPRmass, 40, 240, 5);
+
+  f_fitprmass->SetLineWidth(2);
+  f_fitprmass->SetLineColor(kBlue);
+
+  f_fitprmass->SetParameters(h_prmass->Integral(), -0.107, 139.6, 107.4, h_prmass->GetBinWidth(1));
+  f_fitprmass->FixParameter(0, h_prmass->Integral());
+  f_fitprmass->FixParameter(4, h_prmass->GetBinWidth(1));
+
+  h_prmass->Fit("f_fitprmass", "Q", "", 40, 240);
+  
+  // Calculate the fitting uncertainties by using error matrix
+
+  TFitResultPtr fitptr = h_prmass->Fit(f_fitprmass, "QS");
+  TFitResult fitresult = (*fitptr);
+  TMatrixD corrMatrix  = fitresult.GetCorrelationMatrix();  
+  
+  g_errorBands = fitErrorBand(f_fitprmass, &corrMatrix, fitPRmass);
+
+  g_errorBands->SetFillStyle(1001);
+  g_errorBands->SetFillColor(kYellow);
+
+  /// Fit pruned mass of (side band only) by using the shape parameters from above
+  
+  TF1* f_fitprmass_hollow = new TF1("f_fitprmass_hollow", hollow_fitPRmass, 40, 240, 5);
+
+  f_fitprmass_hollow->SetLineWidth(2);
+  f_fitprmass_hollow->SetLineColor(kBlue);
+
+  f_fitprmass_hollow->FixParameter(1,f_fitprmass->GetParameter(1));
+  f_fitprmass_hollow->FixParameter(2,f_fitprmass->GetParameter(2));
+  f_fitprmass_hollow->FixParameter(3,f_fitprmass->GetParameter(3));
+  f_fitprmass_hollow->FixParameter(4,h_prmass_hollow->GetBinWidth(1));
+
+  h_prmass_hollow->Fit("f_fitprmass_hollow", "Q", "", 40, 240);
+
+  // Calculate the fitting uncertainties by using error matrix
+
+  TFitResultPtr fitptr_hollow = h_prmass_hollow->Fit(f_fitprmass_hollow, "QS");
+  TFitResult fitresult_hollow = (*fitptr_hollow);
+  TMatrixD corrMatrix_hollow  = fitresult_hollow.GetCorrelationMatrix();
+  
+  g_errorBands_hollow = fitErrorBand(f_fitprmass_hollow, &corrMatrix_hollow, hollow_fitPRmass);
+
+  g_errorBands_hollow->SetFillStyle(1001);
+  g_errorBands_hollow->SetFillColor(kYellow);
+
+  /// Fluctuate the pruned mass histogram to test the fitting results
+
+  TH1D* h_fluc = (TH1D*)h_prmass->Clone("h_fluc");
+  TH1D* h_fluc_hollow = (TH1D*)h_prmass->Clone("h_fluc_hollow");
+  
+  for( int ntoy = 0; ntoy < 2000; ntoy++ ){
+
+    h_fluc->Reset();
+    h_fluc_hollow->Reset();
+
+    for( int idata = 0; idata < (int)(h_prmass->Integral()); idata++ )
+      h_fluc->Fill(f_fitprmass->GetRandom(40,240));
+     
+    int nd1 = (105 - h_fluc->GetBinLowEdge(1))/h_fluc->GetBinWidth(1)+1;
+    int nd2 = (135 - h_fluc->GetBinLowEdge(1))/h_fluc->GetBinWidth(1);
+
+    double nSigHist = h_fluc->Integral(nd1,nd2);
+
+    for( int nbin = 1; nbin <= h_fluc->GetNbinsX(); nbin++ ){
+
+      if( h_fluc_hollow->GetBinLowEdge(nbin) < 65 || h_fluc_hollow->GetBinLowEdge(nbin) > 140 ){
+
+	h_fluc_hollow->SetBinContent(nbin,h_fluc->GetBinContent(nbin));
+	h_fluc_hollow->SetBinError(nbin,h_fluc->GetBinError(nbin));
+
+      }
+
+    }
+
+    TF1* f_fluc = new TF1("f_fluc", hollow_fitPRmass, 40, 240, 5);
+
+    f_fluc->FixParameter(1,f_fitprmass->GetParameter(1));
+    f_fluc->FixParameter(2,f_fitprmass->GetParameter(2));
+    f_fluc->FixParameter(3,f_fitprmass->GetParameter(3));
+    f_fluc->FixParameter(4,h_fluc_hollow->GetBinWidth(1));
+
+    h_fluc_hollow->Fit("f_fluc", "Q", "", 40, 240);
+
+    double nSigFit = f_fluc->Integral(105.0,135.0)/h_fluc_hollow->GetBinWidth(1);
+
+    h_bias->Fill((nSigFit - nSigHist)/nSigHist);
+
+    double upfitUnc, dwfitUnc;
+
+    TFitResultPtr fitptr_fluc = h_fluc_hollow->Fit(f_fluc, "QS");
+    TFitResult fitresult_fluc = (*fitptr_fluc);
+    TMatrixD corrMatrix_fluc  = fitresult_fluc.GetCorrelationMatrix();
+
+    fitUncertainty(f_fluc, &corrMatrix_fluc, hollow_fitPRmass, h_fluc_hollow, nSigHist, &upfitUnc, &dwfitUnc);
+
+    h_upPull->Fill((nSigFit - nSigHist)/upfitUnc);
+    h_dwPull->Fill((nSigFit - nSigHist)/dwfitUnc);
+
+  } // end of ntoy loop
+  
+}
--- pseudoMCtest/mu/cat1/toyMCmu_cat1.C
+++ pseudoMCtest/mu/cat1/toyMCmu_cat1.C
@@ -6,68 +6,50 @@
 #include <TClonesArray.h>
 #include <TLorentzVector.h>
 #include "../../../untuplizer.h"
-#include "../../../readSample.h"
-#include "../../../dataFilter.h"
 #include "../../../isPassZmumu.h"
-#include "../../../pileupMCweight.h"
 
 void toyMCmu_cat1(std::string inputFile, std::string outputFile){
 
   // read the ntuples (in pcncu)
 
-  std::vector<string> infiles;
-    
-  readSample(inputFile, infiles);
-  
-  TreeReader data(infiles);
+  TreeReader data(inputFile.data());
 
   // Declare the histogram
+
+  TFile* f = new TFile(inputFile.data());
+  TH1D* h_totalEvents = (TH1D*)f->Get("h_totalEv");
 
   const Double_t xmin  = 500;
   const Double_t xmax  = 5000;
   const Int_t    nBins = (xmax-xmin)/100;
      
-  TH1D* h_ZprimeSign_pMC  = new TH1D("h_ZprimeSign_pMC",   "ZprimeSign_pMC",  nBins, xmin, xmax);
-  TH1D* h_ZprimeSide_pMC  = new TH1D("h_ZprimeSide_pMC",   "ZprimeSide_pMC",  nBins, xmin, xmax);
-  TH1D* h_ZprimeSign_pDA  = new TH1D("h_ZprimeSign_pDA",   "ZprimeSign_pDA",  nBins, xmin, xmax);
-  TH1D* h_ZprimeSide_pDA  = new TH1D("h_ZprimeSide_pDA",   "ZprimeSide_pDA",  nBins, xmin, xmax);
-  TH1D* h_PRmassNoSIG_pMC = new TH1D("h_PRmassNoSIG_pMC",  "PRmassNoSIG_pMC",    40,   40,  240);
-  TH1D* h_PRmassAll_pMC   = new TH1D("h_PRmassAll_pMC",    "PRmassAll_pMC",      40,   40,  240);
-  TH1D* h_PRmassNoSIG_pDA = new TH1D("h_PRmassNoSIG_pDA",  "PRmassNoSIG_pDA",    40,   40,  240);
-  TH1D* h_PRmassAll_pDA   = new TH1D("h_PRmassAll_pDA",    "PRmassAll_pDA",      40,   40,  240);
-  TH1D* h_eventWeight_pMC = new TH1D("h_eventWeight_pMC",  "eventWeight_pMC",     2,   -1,    1);
-  TH1D* h_eventWeight_pDA = new TH1D("h_eventWeight_pDA",  "eventWeight_pDA",     2,   -1,    1);
+  TH1D* h_ZprimeSign  = new TH1D("h_ZprimeSign",  "ZprimeSign",  nBins, xmin, xmax);
+  TH1D* h_ZprimeSide  = new TH1D("h_ZprimeSide",  "ZprimeSide",  nBins, xmin, xmax);
+  TH1D* h_PRmassNoSIG = new TH1D("h_PRmassNoSIG", "PRmassNoSIG",    40,   40,  240);
+  TH1D* h_PRmassAll   = new TH1D("h_PRmassAll",   "PRmassAll",      40,   40,  240);
 
-  h_ZprimeSign_pMC  ->Sumw2();
-  h_ZprimeSide_pMC  ->Sumw2();
-  h_ZprimeSign_pDA  ->Sumw2();
-  h_ZprimeSide_pDA  ->Sumw2();
-  h_PRmassNoSIG_pMC ->Sumw2();
-  h_PRmassAll_pMC   ->Sumw2();
-  h_PRmassNoSIG_pDA ->Sumw2();
-  h_PRmassAll_pDA   ->Sumw2();
+  h_ZprimeSign ->Sumw2();
+  h_ZprimeSide ->Sumw2();
+  h_PRmassNoSIG->Sumw2();
+  h_PRmassAll  ->Sumw2();
 
-  h_ZprimeSign_pMC  ->GetXaxis()->SetTitle("ZprimeSign");
-  h_ZprimeSide_pMC  ->GetXaxis()->SetTitle("ZprimeSide");
-  h_ZprimeSign_pDA  ->GetXaxis()->SetTitle("ZprimeSign");
-  h_ZprimeSide_pDA  ->GetXaxis()->SetTitle("ZprimeSide");
-  h_PRmassNoSIG_pMC ->GetXaxis()->SetTitle("corrPRmass");
-  h_PRmassAll_pMC   ->GetXaxis()->SetTitle("corrPRmass");
-  h_PRmassNoSIG_pDA ->GetXaxis()->SetTitle("corrPRmass");
-  h_PRmassAll_pDA   ->GetXaxis()->SetTitle("corrPRmass");
+  h_ZprimeSign ->GetXaxis()->SetTitle("ZprimeSign");
+  h_ZprimeSide ->GetXaxis()->SetTitle("ZprimeSide");
+  h_PRmassNoSIG->GetXaxis()->SetTitle("corrPRmass");
+  h_PRmassAll  ->GetXaxis()->SetTitle("corrPRmass");
 
   // begin of event loop
 
-  for(Long64_t ev = 0; ev < data.GetEntriesFast(); ev++){
+  Long64_t nentries = data.GetEntriesFast();
+
+  for(Long64_t ev = 0; ev < nentries; ev++){
 
     if( ev % 1000000 == 0 )
-      fprintf(stderr, "Processing event %lli of %lli\n", ev + 1, data.GetEntriesFast());
+      fprintf(stderr, "Processing event %lli of %lli\n", ev + 1, nentries);
 
     data.GetEntry(ev);
 
-    Int_t          nVtx              = data.GetInt("nVtx");
-    Bool_t         isData            = data.GetBool("isData");
-    Float_t        pu_nTrueInt       = data.GetFloat("pu_nTrueInt");
+    Float_t        eventWeight       = data.GetFloat("ev_weight");
     TClonesArray*  muP4              = (TClonesArray*) data.GetPtrTObject("muP4");
     Int_t          FATnJet           = data.GetInt("FATnJet");    
     Int_t*         FATnSubSDJet      = data.GetPtrInt("FATnSubSDJet");
@@ -79,26 +61,6 @@ void toyMCmu_cat1(std::string inputFile, std::string outputFile){
     vector<float>* FATsubjetSDPz     = data.GetPtrVectorFloat("FATsubjetSDPz", FATnJet);
     vector<float>* FATsubjetSDE      = data.GetPtrVectorFloat("FATsubjetSDE", FATnJet);
     vector<float>* FATsubjetSDCSV    = data.GetPtrVectorFloat("FATsubjetSDCSV", FATnJet);
-
-    // remove event which is no hard interaction (noise)
-
-    if( nVtx < 1 ) continue;
-
-    // Correct the pile-up shape of MC
-
-    Double_t eventWeight = pileupWeight(isData, (Int_t)pu_nTrueInt);
-
-    if( ev % 2 == 0 )
-      h_eventWeight_pMC->Fill(0.,eventWeight);
-
-    else if( ev % 2 == 1 )
-      h_eventWeight_pDA->Fill(0.,eventWeight);
-
-
-    // data trigger cut
-      
-    bool muTrigger  = TriggerStatus(data, "HLT_Mu45");
-    if( !muTrigger ) continue;
 
     // select good leptons
       
@@ -170,51 +132,31 @@ void toyMCmu_cat1(std::string inputFile, std::string outputFile){
 
     if( mllbb < 750 ) continue;
 
-    if( ev % 2 == 0 ){
+    h_PRmassAll->Fill(corrPRmass[goodFATJetID],eventWeight);
 
-      h_PRmassAll_pMC->Fill(corrPRmass[goodFATJetID],eventWeight);
+    if( corrPRmass[goodFATJetID] > 30 && !(corrPRmass[goodFATJetID] > 65 && corrPRmass[goodFATJetID] < 135) && corrPRmass[goodFATJetID] < 300 ){
+      h_ZprimeSide ->Fill(mllbb,eventWeight);
+      h_PRmassNoSIG->Fill(corrPRmass[goodFATJetID],eventWeight);
+    }
 
-      if( corrPRmass[goodFATJetID] > 30 && !(corrPRmass[goodFATJetID] > 65 && corrPRmass[goodFATJetID] < 135) && corrPRmass[goodFATJetID] < 300 ){
-	h_ZprimeSide_pMC ->Fill(mllbb,eventWeight);
-	h_PRmassNoSIG_pMC->Fill(corrPRmass[goodFATJetID],eventWeight);
-      }
-
-      if( corrPRmass[goodFATJetID] > 105 && corrPRmass[goodFATJetID] < 135 )
-	h_ZprimeSign_pMC->Fill(mllbb,eventWeight);
+    if( corrPRmass[goodFATJetID] > 105 && corrPRmass[goodFATJetID] < 135 )
+      h_ZprimeSign->Fill(mllbb,eventWeight);
   
-    }
-
-    else if( ev % 2 == 1 ){
-
-      h_PRmassAll_pDA->Fill(corrPRmass[goodFATJetID],eventWeight);
-
-      if( corrPRmass[goodFATJetID] > 30 && !(corrPRmass[goodFATJetID] > 65 && corrPRmass[goodFATJetID] < 135) && corrPRmass[goodFATJetID] < 300 ){
-        h_ZprimeSide_pDA ->Fill(mllbb,eventWeight);
-        h_PRmassNoSIG_pDA->Fill(corrPRmass[goodFATJetID],eventWeight);
-      }
-
-      if( corrPRmass[goodFATJetID] > 105 && corrPRmass[goodFATJetID] < 135 )
-	h_ZprimeSign_pDA->Fill(mllbb,eventWeight);
-
-    }
-
   } // end of event loop
 
   fprintf(stderr, "Processed all events\n");
 
   TFile* outFile = new TFile(Form("%s_pseudoTest.root",outputFile.c_str()), "recreate");
   
-  h_ZprimeSign_pMC   ->Write("ZprimeSign_pMC");
-  h_ZprimeSide_pMC   ->Write("ZprimeSide_pMC");
-  h_PRmassNoSIG_pMC  ->Write("corrPRmass_pMC");
-  h_PRmassAll_pMC    ->Write("corrPRmassAll_pMC");
-  h_ZprimeSign_pDA   ->Write("ZprimeSign_pDA");
-  h_ZprimeSide_pDA   ->Write("ZprimeSide_pDA");
-  h_PRmassNoSIG_pDA  ->Write("corrPRmass_pDA");
-  h_PRmassAll_pDA    ->Write("corrPRmassAll_pDA");
-  h_eventWeight_pMC  ->Write("eventWeight_pMC");
-  h_eventWeight_pDA  ->Write("eventWeight_pDA");
+  h_ZprimeSign ->Write("ZprimeSign");
+  h_ZprimeSide ->Write("ZprimeSide");
+  h_PRmassNoSIG->Write("corrPRmass");
+  h_PRmassAll  ->Write("corrPRmassAll");
+  h_totalEvents->Write("totalEvents");
 
   outFile->Write();
+
+  delete f;
+  delete outFile;
 
 }
