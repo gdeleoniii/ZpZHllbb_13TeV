bf183dc8d93080a201520f591499f3de3cd9af60
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-04-23 16:54:11 +0200

    complete

---
+++ signalEfficiency/getEleEfficiency.h
@@ -0,0 +1,89 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <TClonesArray.h>
+#include <TLorentzVector.h>
+#include "../untuplizer.h"
+#include "../isPassZee.h"
+
+float getEleEfficiency(string inputFile, int cat){
+
+  // read the ntuples (in pcncu)
+
+  TreeReader data(inputFile.data());
+  
+  int passEvent = 0;
+
+  // begin of event loop
+
+  for( Long64_t ev = data.GetEntriesFast()-1; ev >= 0; --ev ){
+
+    data.GetEntry(ev);
+
+    TClonesArray*  eleP4             = (TClonesArray*) data.GetPtrTObject("eleP4");
+    Int_t          FATnJet           = data.GetInt("FATnJet");    
+    Int_t*         FATnSubSDJet      = data.GetPtrInt("FATnSubSDJet");
+    Float_t*       FATjetPRmassCorr  = data.GetPtrFloat("FATjetPRmassL2L3Corr");
+    TClonesArray*  FATjetP4          = (TClonesArray*) data.GetPtrTObject("FATjetP4");
+    vector<bool>&  FATjetPassIDLoose = *((vector<bool>*) data.GetPtr("FATjetPassIDLoose"));
+    vector<float>* FATsubjetSDCSV    = data.GetPtrVectorFloat("FATsubjetSDCSV", FATnJet);
+
+    // select good reco level events     
+    // select good leptons
+      
+    vector<int> goodLepID;
+
+    if( !isPassZee(data,goodLepID) ) continue;
+
+    TLorentzVector* thisLep = (TLorentzVector*)eleP4->At(goodLepID[0]);
+    TLorentzVector* thatLep = (TLorentzVector*)eleP4->At(goodLepID[1]);
+
+    // select good FATjet
+
+    int goodFATJetID = -1;
+    TLorentzVector* thisJet = NULL;
+
+    for( int ij = 0; ij < FATnJet; ++ij ){
+
+      thisJet = (TLorentzVector*)FATjetP4->At(ij);
+
+      if( thisJet->Pt() < 200 ) continue;
+      if( fabs(thisJet->Eta()) > 2.4 ) continue;
+      if( !FATjetPassIDLoose[ij] ) continue;
+      if( thisJet->DeltaR(*thisLep) < 0.8 || thisJet->DeltaR(*thatLep) < 0.8 ) continue;
+      if( FATjetPRmassCorr[ij] < 105 || FATjetPRmassCorr[ij] > 135 ) continue;
+
+      int nsubBjet = 0;
+
+      for( int is = 0; is < FATnSubSDJet[ij]; ++is ){
+
+	if( FATsubjetSDCSV[ij][is] > 0.605 ) ++nsubBjet;
+
+      }
+ 
+      // b-tag cut
+ 
+      if( cat == 1 && nsubBjet != 1 ) continue;
+      if( cat == 2 && nsubBjet != 2 ) continue;
+       
+      goodFATJetID = ij;
+
+      break;
+ 
+    } // end of FatnJet loop
+ 
+    if( goodFATJetID < 0 ) continue;
+
+    if( (*thisLep+*thatLep+*thisJet).M() < 750 ) continue;
+
+    // Noise cleaning?
+
+    if( fabs((*thisLep+*thatLep).Eta()-(*thisLep+*thatLep+*thisJet).Eta()) > 5.0 ) continue;
+
+    ++passEvent;
+
+  } // end of event loop
+  
+  return (float)passEvent/(float)data.GetEntriesFast();
+
+}
---
+++ signalEfficiency/signalEfficiency.C
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <TAxis.h>
+#include <TGraph.h>
+#include <TLatex.h>
+#include <TCanvas.h>
+#include <TLegend.h>
+#include "getEleEfficiency.h"
+#include "getMuEfficiency.h"
+
+const int N = 11;
+
+void signalEfficiency(){
+
+  Float_t x_mzh[N] = {800,1000,1200,1400,1600,1800,2000,2500,3000,3500,4000};
+  Float_t y_eff_elc1[N], y_eff_elc2[N];
+  Float_t y_eff_muc1[N], y_eff_muc2[N];
+
+  for( int i = 0; i < N; ++i ){
+
+    y_eff_elc1[i] = getEleEfficiency(Form("/data7/htong/skim_samples/ele/skim_ele_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root",(Int_t)x_mzh[i]),1);
+    y_eff_elc2[i] = getEleEfficiency(Form("/data7/htong/skim_samples/ele/skim_ele_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root",(Int_t)x_mzh[i]),2);
+    y_eff_muc1[i] = getEleEfficiency(Form("/data7/htong/skim_samples/mu/skim_mu_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root",(Int_t)x_mzh[i]),1);
+    y_eff_muc2[i] = getEleEfficiency(Form("/data7/htong/skim_samples/mu/skim_mu_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root",(Int_t)x_mzh[i]),2);
+
+  }
+
+  TGraph *g_eff_elc1 = new TGraph(N, x_mzh, y_eff_elc1);
+  TGraph *g_eff_elc2 = new TGraph(N, x_mzh, y_eff_elc2);
+  TGraph *g_eff_muc1 = new TGraph(N, x_mzh, y_eff_muc1);
+  TGraph *g_eff_muc2 = new TGraph(N, x_mzh, y_eff_muc2);
+
+  g_eff_elc1->SetMinimum(0);
+  g_eff_elc1->SetMaximum(0.5);
+  g_eff_elc1->SetTitle("");
+  g_eff_elc1->GetXaxis()->SetTitle("m_{ZH} (GeV)");
+  g_eff_elc1->GetYaxis()->SetTitle("Acceptance #times efficiency");  
+  g_eff_elc1->GetYaxis()->SetTitleOffset(1.3);
+
+  g_eff_elc1->SetLineWidth(2);
+  g_eff_elc1->SetLineColor(kBlue);
+  g_eff_elc1->SetMarkerColor(kBlue);
+  g_eff_elc1->SetMarkerStyle(8);
+
+  g_eff_elc2->SetLineStyle(3);
+  g_eff_elc2->SetLineWidth(2);
+  g_eff_elc2->SetLineColor(kBlue);
+  g_eff_elc2->SetMarkerColor(kBlue);
+  g_eff_elc2->SetMarkerStyle(8);
+
+  g_eff_muc1->SetLineWidth(2);
+  g_eff_muc1->SetLineColor(kRed);
+  g_eff_muc1->SetMarkerColor(kRed);
+  g_eff_muc1->SetMarkerStyle(8);
+
+  g_eff_muc2->SetLineStyle(3);
+  g_eff_muc2->SetLineWidth(2);
+  g_eff_muc2->SetLineColor(kRed);
+  g_eff_muc2->SetMarkerColor(kRed);
+  g_eff_muc2->SetMarkerStyle(8);
+    
+  TLegend leg(0.73, 0.80, 0.90, 0.87);
+  
+  leg.SetBorderSize(0);
+  leg.SetFillColor(0);
+  leg.SetFillStyle(0);
+  leg.SetTextSize(0.04);
+  leg.AddEntry(g_eff_elc1, "1 b-tag", "lp");
+  leg.AddEntry(g_eff_elc2, "2 b-tag", "lp");
+  leg.AddEntry(g_eff_muc1, "1 b-tag", "lp");
+  leg.AddEntry(g_eff_muc2, "2 b-tag", "lp");
+
+  TLatex lar;
+
+  lar.SetNDC(kTRUE);
+  lar.SetTextSize(0.04);
+  lar.SetLineWidth(5);
+
+  TCanvas c("c", "", 0, 0, 800, 600);
+
+  c.cd();
+  g_eff_elc1->Draw();
+  g_eff_elc2->Draw("lpsame");
+  g_eff_muc1->Draw("lpsame");
+  g_eff_muc2->Draw("lpsame");
+  leg.Draw();
+  lar.DrawLatex(0.15, 0.83, "CMS #it{#bf{Simulation}}");
+  c.Print("signalEfficiency.pdf");
+
+}
