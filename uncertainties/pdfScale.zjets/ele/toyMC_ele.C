b5e109ae36d9ce2a9a03646dd1a9c7738e4cf1c0
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-06-12 15:35:49 +0200

    first commit

---
+++ uncertainties/pdfScale.zjets/ele/toyMC_ele.C
@@ -0,0 +1,142 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <TH1D.h>
+#include <TFile.h>
+#include <TClonesArray.h>
+#include <TLorentzVector.h>
+#include "../../../untuplizer.h"
+#include "../../../isPassZee.h"
+
+float crossSection(string thisPath){
+
+  ifstream textFile("/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/xSec.txt");
+  string token;
+  float crosssection = 0., thisNum = 0.;
+
+  while( textFile >> token >> thisNum ){
+
+    if( thisPath.find(token) != string::npos )
+      crosssection = thisNum;
+
+  }
+
+  return crosssection;
+
+}
+
+void toyMC_ele(string inputFile, string outputFile){
+
+  // read the ntuples (in pcncu)
+
+  TreeReader data(inputFile.data());
+
+  TFile* f = new TFile(inputFile.data());
+  TH1D* h_totalEvents = (TH1D*)f->Get("h_totalEv");
+
+  // Create a tree to store variables
+
+  TFile* outFile = new TFile(Form("%s_toyMC.root",outputFile.c_str()), "recreate");
+  TTree* tree = new TTree("tree", "TreeForRooFit");
+
+  Int_t   cat;
+  Float_t mllbb, prmass;
+  Float_t evweight[999] = {0};
+
+  tree->Branch("cat",      &cat,     "cat/I");
+  tree->Branch("mllbb",    &mllbb,   "mllbb/F");
+  tree->Branch("prmass",   &prmass,  "prmass/F");
+  tree->Branch("evweight", evweight, "evweight[999]/F");
+
+  // Calculate the scale correspond to inputFile
+
+  Float_t scale = 2512.*crossSection(outputFile.data())/h_totalEvents->Integral();
+
+  // begin of event loop
+
+  fprintf(stdout, "Total events %lli\n", data.GetEntriesFast());
+
+  for( Long64_t ev = data.GetEntriesFast()-1; ev >= 0; --ev ){
+
+    if( (unsigned)ev % 100000 == 0 )
+      fprintf(stdout, "Still left events %lli\n", ev);
+
+    data.GetEntry(ev);
+
+    Float_t*       pdfscaleSysWeight = data.GetPtrFloat("pdfscaleSysWeights"); 
+    Float_t        eventWeight       = data.GetFloat("ev_weight"); 
+    TClonesArray*  eleP4             = (TClonesArray*) data.GetPtrTObject("eleP4");
+    Int_t          FATnJet           = data.GetInt("FATnJet");    
+    Int_t*         FATnSubSDJet      = data.GetPtrInt("FATnSubSDJet");
+    Float_t*       corrPRmass        = data.GetPtrFloat("FATjetPRmassL2L3Corr");
+    TClonesArray*  FATjetP4          = (TClonesArray*) data.GetPtrTObject("FATjetP4");
+    vector<bool>&  FATjetPassIDLoose = *((vector<bool>*) data.GetPtr("FATjetPassIDLoose"));
+    vector<float>* FATsubjetSDCSV    = data.GetPtrVectorFloat("FATsubjetSDCSV", FATnJet);
+
+    // select good leptons
+      
+    vector<int> goodLepID;
+    if( !isPassZee(data,goodLepID) ) continue;
+
+    TLorentzVector* thisLep = (TLorentzVector*)eleP4->At(goodLepID[0]);   
+    TLorentzVector* thatLep = (TLorentzVector*)eleP4->At(goodLepID[1]);   
+
+    // select good FATjet
+
+    Int_t goodFATJetID = -1;
+    TLorentzVector* thisJet = NULL;
+
+    for( Int_t ij = 0; ij < FATnJet; ++ij ){
+
+      thisJet = (TLorentzVector*)FATjetP4->At(ij);
+
+      if( thisJet->Pt() < 200 ) continue;
+      if( fabs(thisJet->Eta()) > 2.4 ) continue;
+      if( !FATjetPassIDLoose[ij] ) continue;
+      if( thisJet->DeltaR(*thisLep) < 0.8 || thisJet->DeltaR(*thatLep) < 0.8 ) continue;
+      
+      Int_t nsubBjet = 0;
+
+      for( Int_t is = 0; is < FATnSubSDJet[ij]; ++is ){
+
+	if( FATsubjetSDCSV[ij][is] > 0.605 ) nsubBjet++;
+
+      }
+
+      // b-tag cut
+
+      if     ( nsubBjet == 1 ) cat = 1;
+      else if( nsubBjet == 2 ) cat = 2;      
+      else                     cat = 0;
+
+      goodFATJetID = ij;
+      break;
+
+    } // end of FatnJet loop
+
+    if( goodFATJetID < 0 ) continue;
+
+    mllbb = (*thisLep+*thatLep+*thisJet).M();  
+
+    if( mllbb < 750 ) continue;
+
+    prmass = corrPRmass[goodFATJetID];
+
+    for( int i = 10; i < 110; ++i ){
+      evweight[i] = eventWeight * scale * pdfscaleSysWeight[i];
+    }
+
+    tree->Fill();
+
+  } // end of event loop
+
+  fprintf(stdout, "Processed all events\n");
+
+  tree->Write();  
+  outFile->Write();
+
+  delete f;
+  delete outFile;
+
+}
---
+++ uncertainties/pdfScale.zjets/rooFitData.C
@@ -0,0 +1,123 @@
+R__LOAD_LIBRARY(PDFs/HWWLVJRooPdfs_cxx.so)
+R__LOAD_LIBRARY(PDFs/PdfDiagonalizer_cc.so)
+using namespace RooFit;
+
+void rooFitData(string channel, string catcut){
+
+  // Suppress all the INFO message
+
+  RooMsgService::instance().setGlobalKillBelow(RooFit::FATAL);
+
+  // Input files and sum all backgrounds
+
+  TChain* treeZjets = new TChain("tree");
+
+  treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-100to200_13TeV_toyMC.root", channel.data()));
+  treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-200to400_13TeV_toyMC.root", channel.data()));
+  treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-400to600_13TeV_toyMC.root", channel.data()));
+  treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-600toInf_13TeV_toyMC.root", channel.data()));
+
+  // Define all the variables from the trees
+
+  RooRealVar cat ("cat", "", 0, 2);
+  RooRealVar mJet("prmass", "M_{jet}",  30.,  300., "GeV");
+  RooRealVar mZH ("mllbb",   "M_{ZH}", 900., 3000., "GeV");
+  RooRealVar evWeight("evweight[1]", "", -1.e10, 1.e10);
+
+  // Set the range in zh mass 
+
+  mZH.setRange("fullRange", 900., 3000.);
+
+  RooBinning binsmZH(21, 900, 3000);
+
+  RooArgSet variables(cat, mJet, mZH, evWeight);
+
+  TCut catCut = Form("cat==%s", catcut.c_str());
+  TCut sbCut  = "prmass>30 && !(prmass>65 && prmass<135) && prmass<300";
+  TCut sigCut = "prmass>105 && prmass<135";
+
+  // Create a dataset from a tree -> to process an unbinned likelihood fitting
+
+  RooDataSet dataSetZjetsSB("dataSetZjetsSB", "dataSetZjetsSB", variables, Cut(catCut && sbCut),  WeightVar(evWeight), Import(*treeZjets));  
+  RooDataSet dataSetZjetsSG("dataSetZjetsSG", "dataSetZjetsSG", variables, Cut(catCut && sigCut), WeightVar(evWeight), Import(*treeZjets));
+  
+  // Total events number
+
+  float totalSBMcEv = dataSetZjetsSB.sumEntries();
+  float totalSGMcEv = dataSetZjetsSG.sumEntries();
+
+  RooRealVar nSBMcEvents("nSBMcEvents", "nSBMcEvents", 0., 9999999.);
+  RooRealVar nSGMcEvents("nSGMcEvents", "nSGMcEvents", 0., 9999999.);
+
+  nSBMcEvents.setVal(totalSBMcEv);
+  nSBMcEvents.setConstant(true);
+  
+  nSGMcEvents.setVal(totalSGMcEv);
+  nSGMcEvents.setConstant(true);
+  
+  // Alpha ratio part
+
+  RooRealVar a("a", "a",  0., -1.,    1.);
+  RooRealVar b("b", "b", 1000,  0., 4000.);
+  
+  RooGenericPdf model_ZHSB("model_ZHSB", "model_ZHSB", "TMath::Exp(@1*@0+@2/@0)", RooArgSet(mZH,a,b));
+  RooGenericPdf model_ZHSG("model_ZHSG", "model_ZHSG", "TMath::Exp(@1*@0+@2/@0)", RooArgSet(mZH,a,b));
+
+  RooExtendPdf ext_model_ZHSB("ext_model_ZHSB", "ext_model_ZHSB", model_ZHSB, nSBMcEvents);
+  RooExtendPdf ext_model_ZHSG("ext_model_ZHSG", "ext_model_ZHSG", model_ZHSG, nSGMcEvents);
+
+  // Fit ZH mass in side band  
+
+  RooFitResult* mZHSB_result = ext_model_ZHSB.fitTo(dataSetZjetsSB, SumW2Error(true), Extended(true), Range("fullRange"), Strategy(2), Minimizer("Minuit2"), Save(1));
+
+  RooAbsReal* nZHSBFit = ext_model_ZHSB.createIntegral(RooArgSet(mZH), NormSet(mZH), Range("fullRange"));
+
+  float p0 = a.getVal();
+  float p1 = b.getVal();
+
+  // Fit ZH mass in signal region
+
+  RooFitResult* mZHSG_result = ext_model_ZHSG.fitTo(dataSetZjetsSG, SumW2Error(true), Extended(true), Range("fullRange"), Strategy(2), Minimizer("Minuit2"), Save(1));
+
+  RooAbsReal* nZHSGFit = ext_model_ZHSG.createIntegral(RooArgSet(mZH), NormSet(mZH), Range("fullRange"));
+
+  float p2 = a.getVal();
+  float p3 = b.getVal();
+
+  // Draw the model of alpha ratio
+
+  RooGenericPdf model_alpha("model_alpha", "model_alpha", Form("TMath::Exp(%f*@0+%f/@0)/TMath::Exp(%f*@0+%f/@0)", p2,p3,p0,p1), RooArgSet(mZH));
+
+  // Plot the results to a frame 
+
+  RooPlot* alphaFrame = mZH.frame();
+
+  model_alpha.plotOn(alphaFrame, LineColor(kBlue));
+  /*
+  TLegend* leg0 = new TLegend(0.60,0.67,0.85,0.85);
+
+  leg0->AddEntry(mZHFrameMC->findObject(mZHFrameMC->nameOf(2)), "MC side band", "lep");
+  leg0->AddEntry(mZHFrameMC->findObject(mZHFrameMC->nameOf(6)), "MC signal region", "lep");
+  leg0->Draw();
+
+  mZHFrameMC->addObject(leg0);
+  mZHFrameMC->SetMinimum(0);
+  */  
+  alphaFrame->SetTitle("");
+  alphaFrame->GetYaxis()->SetTitle("#alpha Ratio");
+
+  TLatex* lar = new TLatex();
+
+  lar->SetTextSize(0.035);
+  lar->SetLineWidth(5);
+  
+  TCanvas* c = new TCanvas("c","",0,0,1000,800);
+
+  c->cd();
+  alphaFrame->Draw();
+  lar->DrawLatexNDC(0.12, 0.92, "CMS #it{#bf{2015}}");
+  lar->DrawLatexNDC(0.55, 0.92, "L = 2.512 fb^{-1} at #sqrt{s} = 13 TeV");
+  lar->DrawLatexNDC(0.75, 0.80, Form("%s  %s btag", channel.data(), catcut.data()));
+  c->Print(Form("alpha_pdfScale_%s_cat%s.pdf", channel.data(), catcut.data()));
+
+}
