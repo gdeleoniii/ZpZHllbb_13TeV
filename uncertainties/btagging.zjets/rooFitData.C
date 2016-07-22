474aba244024332b42d789896af813193c5a5c63
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-07-22 19:27:13 +0200

    update

--- uncertainties/btagging.zjets/rooFitData.C
+++ uncertainties/btagging.zjets/rooFitData.C
@@ -9,34 +9,32 @@ void rooFitData(string channel, string catcut){
   RooMsgService::instance().setGlobalKillBelow(RooFit::FATAL);
   RooMsgService::instance().setSilentMode(true);
 
-  // Input files and sum all backgrounds
-
-  TChain* treeZjets = new TChain("tree");
-
-  treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-100to200_13TeV_toyMC.root", channel.data()));
-  treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-200to400_13TeV_toyMC.root", channel.data()));
-  treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-400to600_13TeV_toyMC.root", channel.data()));
-  treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-600toInf_13TeV_toyMC.root", channel.data()));
-
-  RooRealVar mZH ("mllbb", "M_{ZH}", 900., 3000., "GeV");
-  
+  string region[3] = {"central","up","down"};
+  RooRealVar mZH("mllbb", "M_{ZH}", 900., 3000., "GeV");
   RooPlot* alphaFrame = mZH.frame();
-  
+
   for(int nw = 2; nw >= 0; --nw){
 
     fprintf(stdout, "Using weight %i\n", nw);
 
+    // Input files and sum all backgrounds
+
+    TChain* treeZjets = new TChain("tree");
+    
+    treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-100to200_13TeV_%s_toyMC.root", channel.data(), region[nw].data()));
+    treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-200to400_13TeV_%s_toyMC.root", channel.data(), region[nw].data()));
+    treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-400to600_13TeV_%s_toyMC.root", channel.data(), region[nw].data()));
+    treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-600toInf_13TeV_%s_toyMC.root", channel.data(), region[nw].data()));
+    
     // Define all the variables from the trees 
 
     RooRealVar cat ("cat", "", 0, 2);
-    RooRealVar mJet("prmass", "M_{jet}",  30.,  300., "GeV");
-    RooRealVar evWeight(Form("evweight%i",nw), "", -1.e10, 1.e10);
+    RooRealVar mJet("prmass", "", 30., 300.);
+    RooRealVar evWeight("evweight", "", 0., 1.e10);
   
     // Set the range in zh mass 
 
     mZH.setRange("fullRange", 900., 3000.);
-
-    RooBinning binsmZH(21, 900, 3000);
 
     RooArgSet variables(cat, mJet, mZH, evWeight);
 
@@ -48,7 +46,7 @@ void rooFitData(string channel, string catcut){
 
     RooDataSet dataSetZjetsSB("dataSetZjetsSB", "dataSetZjetsSB", variables, Cut(catCut && sbCut),  WeightVar(evWeight), Import(*treeZjets));  
     RooDataSet dataSetZjetsSG("dataSetZjetsSG", "dataSetZjetsSG", variables, Cut(catCut && sigCut), WeightVar(evWeight), Import(*treeZjets));
-  
+
     // Total events number
 
     float totalSBMcEv = dataSetZjetsSB.sumEntries();
@@ -65,19 +63,27 @@ void rooFitData(string channel, string catcut){
   
     // Alpha ratio part
 
-    RooRealVar a("a", "a", -0.0005, -1.,     1.);
-    RooRealVar b("b", "b",    1200,  0., 10000.);
-  
+    // Fit ZH mass in side band 
+
+    float bmin, bmax;
+
+    if( channel == "ele" ){
+      bmin = (catcut=="1") ? 1300. : 0.;
+      bmax = (catcut=="1") ? 1800. : 2.;
+    }
+
+    else if( channel == "mu" ){
+      bmin = 600.; 
+      bmax = 1200.;
+    }
+    
+    RooRealVar a("a", "a", -0.002, -0.005, 0.);
+    RooRealVar b("b", "b", (bmin+bmax)*0.5, bmin, bmax);
+
     RooGenericPdf model_ZHSB("model_ZHSB", "model_ZHSB", "TMath::Exp(@1*@0+@2/@0)", RooArgSet(mZH,a,b));
-    RooGenericPdf model_ZHSG("model_ZHSG", "model_ZHSG", "TMath::Exp(@1*@0+@2/@0)", RooArgSet(mZH,a,b));
-
     RooExtendPdf ext_model_ZHSB("ext_model_ZHSB", "ext_model_ZHSB", model_ZHSB, nSBMcEvents);
-    RooExtendPdf ext_model_ZHSG("ext_model_ZHSG", "ext_model_ZHSG", model_ZHSG, nSGMcEvents);
-
-    // Fit ZH mass in side band  
 
     RooFitResult* mZHSB_result = ext_model_ZHSB.fitTo(dataSetZjetsSB, SumW2Error(true), Extended(true), Range("fullRange"), Strategy(2), Minimizer("Minuit2"), Save(1));
-
     RooAbsReal* nZHSBFit = ext_model_ZHSB.createIntegral(RooArgSet(mZH), NormSet(mZH), Range("fullRange"));
 
     float p0 = a.getVal();
@@ -85,20 +91,41 @@ void rooFitData(string channel, string catcut){
 
     // Fit ZH mass in signal region
 
-    RooFitResult* mZHSG_result = ext_model_ZHSG.fitTo(dataSetZjetsSG, SumW2Error(true), Extended(true), Range("fullRange"), Strategy(2), Minimizer("Minuit2"), Save(1));
+    float dmin, dmax;
+    
+    if( channel == "ele" ){
+      dmin = (catcut=="1") ? 800.  : 2100.;
+      dmax = (catcut=="1") ? 1400. : 2800.;
+    }
+    
+    else if( channel == "mu" ){
+      dmin = (catcut=="1") ? 0. : 1500.;
+      dmax = (catcut=="1") ? 1. : 2500.;
+    }
+    
+    RooRealVar c("c", "c", -0.002, -0.005, 0.);
+    RooRealVar d("d", "d", (dmin+dmax)*0.5, dmin, dmax);
 
+    RooGenericPdf model_ZHSG("model_ZHSG", "model_ZHSG", "TMath::Exp(@1*@0+@2/@0)", RooArgSet(mZH,c,d));
+    RooExtendPdf ext_model_ZHSG("ext_model_ZHSG", "ext_model_ZHSG", model_ZHSG, nSGMcEvents);
+
+    RooFitResult* mZHSG_result = ext_model_ZHSG.fitTo(dataSetZjetsSG, SumW2Error(true), Extended(true), Range("fullRange"), Strategy(2), Minimizer("Minuit2"), Save(1));
     RooAbsReal* nZHSGFit = ext_model_ZHSG.createIntegral(RooArgSet(mZH), NormSet(mZH), Range("fullRange"));
 
-    float p2 = a.getVal();
-    float p3 = b.getVal();
+    float p2 = c.getVal();
+    float p3 = d.getVal();
 
     // Draw the model of alpha ratio
 
     RooGenericPdf model_alpha("model_alpha", "model_alpha", Form("TMath::Exp(%f*@0+%f/@0)/TMath::Exp(%f*@0+%f/@0)", p2,p3,p0,p1), RooArgSet(mZH));
 
+    fprintf(stdout, "p0=%f\tp1=%f\tp2=%f\tp3=%f\n", p0,p1,p2,p3);
+
     // Plot the results to a frame 
 
     model_alpha.plotOn(alphaFrame, LineColor((nw==0)?kBlue:kYellow));
+
+    delete treeZjets;
 
   }
 
@@ -113,9 +140,9 @@ void rooFitData(string channel, string catcut){
 
   alphaFrame->addObject(leg);
   alphaFrame->SetTitle("");
-  alphaFrame->SetMaximum(0.03);
   alphaFrame->GetYaxis()->SetTitle("#alpha Ratio");
   alphaFrame->GetYaxis()->SetTitleOffset(1.3);
+  alphaFrame->SetMaximum(0.03);
 
   TLatex* lar = new TLatex();
 
