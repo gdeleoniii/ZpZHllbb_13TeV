a653898ddde7341c70796edf82ebf18671f12de8
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-07-26 10:20:39 +0200

    test

--- uncertainties/pdfScale.zjets/rooFitData.C
+++ uncertainties/pdfScale.zjets/rooFitData.C
@@ -18,19 +18,18 @@ void rooFitData(string channel, string catcut, string type, int first, int last,
   treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-400to600_13TeV_toyMC.root", channel.data()));
   treeZjets->Add(Form("%s/Zjets/DYJetsToLL_M-50_HT-600toInf_13TeV_toyMC.root", channel.data()));
 
-  RooRealVar mZH("mllbb", "M_{ZH}", 900., 3000., "GeV");
+  RooRealVar mZH("mllbb", "M_{ZH}", 800., 4000., "GeV");
   
-  RooPlot* alphaFrame = mZH.frame();
   RooPlot* mZHsbFrame = mZH.frame();
   RooPlot* mZHsgFrame = mZH.frame();
 
-  RooBinning mZHbin(21, 900., 3000.);
+  RooBinning mZHbin(32, 800., 4000.);
   
-  TFile* output = new TFile(Form("alpha_%sScale_%s_cat%s.root", type.data(), channel.data(), catcut.data()), "recreate");
+  const int N = 1+(last-first)/iter;
+  int iw = N-1;
+  float alpha[11][N];
 
   for(int nw = last; nw >= first; nw -= iter){
-
-    // fprintf(stdout, ">>>> Weight %i <<<<\n", nw);
 
     // Define all the variables from the trees 
 
@@ -40,7 +39,7 @@ void rooFitData(string channel, string catcut, string type, int first, int last,
   
     // Set the range in zh mass 
 
-    mZH.setRange("fullRange", 900., 3000.);
+    mZH.setRange("fullRange", 800., 4000.);
 
     RooArgSet variables(cat, mJet, mZH, evWeight);
 
@@ -74,13 +73,13 @@ void rooFitData(string channel, string catcut, string type, int first, int last,
     float bmin, bmax;
 
     if( channel == "ele" ){
-      bmin = (catcut=="1") ? 1300. : 0.;
-      bmax = (catcut=="1") ? 1800. : 2.;
+      bmin = (catcut=="1") ?  600. : 1500.;
+      bmax = (catcut=="1") ? 1200. : 2500.;
     }
 
     else if( channel == "mu" ){
-      bmin = 600.; 
-      bmax = 1200.;
+      bmin = (catcut=="1") ? 100. : 2000.; 
+      bmax = (catcut=="1") ? 700. : 2700.;
     }
     
     RooRealVar a("a", "a", -0.002, -0.005, 0.);
@@ -100,13 +99,13 @@ void rooFitData(string channel, string catcut, string type, int first, int last,
     float dmin, dmax;
     
     if( channel == "ele" ){
-      dmin = (catcut=="1") ? 800.  : 2100.;
-      dmax = (catcut=="1") ? 1400. : 2800.;
+      dmin = (catcut=="1") ? 3000. : 0.;
+      dmax = (catcut=="1") ? 3900. : 1.;
     }
     
     else if( channel == "mu" ){
-      dmin = (catcut=="1") ? 0. : 1500.;
-      dmax = (catcut=="1") ? 1. : 2500.;
+      dmin = (catcut=="1") ? 0. : 10.;
+      dmax = (catcut=="1") ? 1. : 15.;
     }
     
     RooRealVar c("c", "c", -0.002, -0.005, 0.);
@@ -121,51 +120,74 @@ void rooFitData(string channel, string catcut, string type, int first, int last,
     float p2 = c.getVal();
     float p3 = d.getVal();
 
-    // Draw the model of alpha ratio
+    // Set the model of alpha ratio and save it
 
-    RooGenericPdf model_alpha("model_alpha", "model_alpha", Form("TMath::Exp(%f*@0+%f/@0)/TMath::Exp(%f*@0+%f/@0)", p2,p3,p0,p1), RooArgSet(mZH));
+    TF1* model_alpha = new TF1("ftest", "TMath::Exp([0]*x+[1]/x)/TMath::Exp([2]*x+[3]/x)", 800, 4000);
 
-    TF1* f_alpha = (TF1*)model_alpha.asTF(RooArgList(mZH),RooArgList(),mZH);
+    model_alpha->SetParameters(p2,p3,p0,p1);
+    model_alpha->SetTitle("");
 
-    f_alpha->Write(Form("alpha%02i",nw));
+    int mzh = 800;
 
-    fprintf(stdout, "p0=%f\tp1=%f\tp2=%f\tp3=%f\n", p0,p1,p2,p3);
+    for( int im = 0; im < 11; ++im ){
+
+      alpha[im][iw] = model_alpha->Eval(mzh);
+      mzh += (mzh<2000) ? 200 : 500;
+
+    }
+
+    --iw;
 
     // Plot the results to a frame 
-
-    model_alpha.plotOn(alphaFrame, LineColor((nw==first)?kBlue:kYellow));
-
+    
     dataSetZjetsSB.plotOn(mZHsbFrame, Binning(mZHbin), MarkerColor((nw==first)?kBlue:kCyan), LineColor((nw==first)?kBlue:kCyan));
     model_ZHSB.plotOn(mZHsbFrame, Range("fullRange"), LineColor((nw==first)?kBlue:kCyan));
 
     dataSetZjetsSG.plotOn(mZHsgFrame, Binning(mZHbin), MarkerColor((nw==first)?kBlue:kCyan), LineColor((nw==first)?kBlue:kCyan));
     model_ZHSG.plotOn(mZHsgFrame, Range("fullRange"), LineColor((nw==first)?kBlue:kCyan));
+    
+    fprintf(stdout, "weight=%i\tp0=%f\tp1=%f\tp2=%f\tp3=%f\n", nw, p0, p1, p2, p3);
 
-  } // end of weight for loop
+    delete model_alpha;
 
-  output->Write();
+  } // end of weight loop
+  
+  // Calculate RMS value of each mass bin
 
-  TLegend* leg = new TLegend(0.15,0.15,0.30,0.25);
+  float Mzh[11]   = {800,1000,1200,1400,1600,1800,2000,2500,3000,3500,4000};
+  float Alpha[11], Uncp[11], Uncm[11];
 
-  leg->SetBorderSize(0);
-  leg->SetFillColor(0);
-  leg->SetFillStyle(0);
-  leg->SetTextSize(0.04);
-  leg->AddEntry(alphaFrame->findObject(alphaFrame->nameOf(0)), "central values", "l");
-  leg->Draw();
+  for( int im = 0; im < 11; ++im ){
 
-  alphaFrame->addObject(leg);
-  alphaFrame->SetTitle("");
-  alphaFrame->SetMaximum(0.03);
-  alphaFrame->GetYaxis()->SetTitle("#alpha Ratio");
-  alphaFrame->GetYaxis()->SetTitleOffset(1.3);
+    Alpha[im] = alpha[im][0];
+    Uncp[im] = (type=="mur1") ? fabs(alpha[im][1]-alpha[im][0]) : TMath::RMS(N, alpha[im]);
+    Uncm[im] = (type=="mur1") ? fabs(alpha[im][2]-alpha[im][0]) : Uncp[im];
+
+  }
+
+  TGraphAsymmErrors *g = new TGraphAsymmErrors(11, Mzh, Alpha);
+
+  g->SetTitle("");
+  g->GetXaxis()->SetTitle("m_{ZH} (GeV)");
+  g->GetYaxis()->SetTitle("#alpha Ratio");  
+  g->GetYaxis()->SetTitleOffset(1.3);
+  g->SetMinimum(0);
+
+  TGraphAsymmErrors *g_alpha = new TGraphAsymmErrors(11, Mzh, Alpha, 0, 0, Uncp, Uncm);
+
+  g_alpha->SetLineWidth(2);
+  g_alpha->SetLineColor(kBlue);
+  g_alpha->SetMarkerStyle(8);
+  g_alpha->SetMarkerColor(kBlue);
+  g_alpha->SetFillStyle(1001);
+  g_alpha->SetFillColor(kYellow);  
 
   mZHsbFrame->SetTitle("");
   mZHsbFrame->SetMinimum(0);
 
   mZHsgFrame->SetTitle("");
   mZHsgFrame->SetMinimum(0);
-
+  
   TLatex* lar = new TLatex();
 
   lar->SetTextSize(0.035);
@@ -174,13 +196,14 @@ void rooFitData(string channel, string catcut, string type, int first, int last,
   TCanvas* c = new TCanvas("c","",0,0,1000,800);
 
   c->cd();
-  alphaFrame->Draw();
+  g->Draw();
+  g_alpha->Draw("3same");
+  g_alpha->Draw("cxsame");
   lar->DrawLatexNDC(0.12, 0.92, "CMS #it{#bf{2015}}");
   lar->DrawLatexNDC(0.55, 0.92, "L = 2.512 fb^{-1} at #sqrt{s} = 13 TeV");
   lar->DrawLatexNDC(0.72, 0.80, Form("%s  %s btag", channel.data(), catcut.data()));
-  lar->DrawLatexNDC(0.72, 0.75, Form("%s", (first==0)?"mur = 1":type.data()));
-  c->Print(Form("alpha_%sScale_%s_cat%s.pdf(", type.data(), channel.data(), catcut.data()));
-
+  c->Print(Form("alpha_%sScale_%s_cat%s.pdf(", type.data(), channel.data(), catcut.data()));  
+  
   c->cd();
   mZHsbFrame->Draw();
   lar->DrawLatexNDC(0.12, 0.92, "CMS #it{#bf{2015}}");
