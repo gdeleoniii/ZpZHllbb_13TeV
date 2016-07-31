3239b962f2f003693acd716d682e1daf62997d2c
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-07-31 12:27:23 +0200

    update

--- uncertainties/jetEnergyScale.signal/jetEnergyScale.C
+++ uncertainties/jetEnergyScale.signal/jetEnergyScale.C
@@ -1,38 +1,44 @@
-#include <iostream>
-#include <cstdio>
+#include <fstream>
+#include <TSystem.h>
 #include "eleJetEnergyScale.h"
 #include "muJetEnergyScale.h"
 
 void jetEnergyScale(){
 
-  int mzh[11] = {800,1000,1200,1400,1600,1800,2000,2500,3000,3500,4000};
+  float mzh[11] = {800,1000,1200,1400,1600,1800,2000,2500,3000,3500,4000};
 
   for( int cat = 1; cat <= 2; ++cat ){
 
-    FILE* fe = fopen(Form("eb%d_jetEnergyUnc.txt",cat), "w");
-    FILE* fm = fopen(Form("mb%d_jetEnergyUnc.txt",cat), "w");
+    FILE* fe = fopen(Form("ele_%ibtag_jesUnc.txt", cat), "w");
+    FILE* fm = fopen(Form("mu_%ibtag_jesUnc.txt", cat), "w");
 
-    // mass, central, up, down
+    fprintf(fe, "mass\tcentral\tJESuncUp\tJESuncDw\n");
+    fprintf(fm, "mass\tcentral\tJESuncUp\tJESuncDw\n");
+
+    float jes0e[11], jesUpe[11], jesDwe[11];
+    float jes0m[11], jesUpm[11], jesDwm[11];
 
     for( int i = 0; i < 11; ++i ){
-    
-      fprintf(fe, "%d\t%g\t%g\t%g\n",
-	      mzh[i],
-	      eleJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_ele_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", mzh[i]), cat, 0),
-	      eleJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_ele_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", mzh[i]), cat, 1),
-	      eleJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_ele_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", mzh[i]), cat, 2));
 
-      fprintf(fm, "%d\t%g\t%g\t%g\n",
-	      mzh[i],
-	      muJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_mu_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", mzh[i]), cat, 0),
-	      muJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_mu_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", mzh[i]), cat, 1),
-	      muJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_mu_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", mzh[i]), cat, 2));
-    
+      jes0e[i]  = eleJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_ele_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", (int)mzh[i]), "central", cat);
+      jesUpe[i] = fabs( eleJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_ele_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", (int)mzh[i]), "up", cat) - jes0e[i] );
+      jesDwe[i] = fabs( eleJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_ele_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", (int)mzh[i]), "dw", cat) - jes0e[i] );
+
+      jes0m[i]  = muJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_mu_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", (int)mzh[i]), "central", cat);
+      jesUpm[i] = fabs( muJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_mu_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", (int)mzh[i]), "up", cat) - jes0m[i] );
+      jesDwm[i] = fabs( muJetEnergyScale(Form("/data7/htong/skim_NCUGlobalTuples/skim_mu_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root", (int)mzh[i]), "dw", cat) - jes0m[i] );
+
+      fprintf(fe, "%i\t%g\t%g\t%g\n", (int)mzh[i], jes0e[i], jesUpe[i], jesDwe[i]);      
+      fprintf(fm, "%i\t%g\t%g\t%g\n", (int)mzh[i], jes0m[i], jesUpm[i], jesDwm[i]);
+
     }
 
     fclose(fe);
     fclose(fm);
 
   }
+
+  gSystem->Exec("mkdir signalJetEnScaleResults");
+  gSystem->Exec("mv *txt signalJetEnScaleResults");
 
 }
--- uncertainties/jetEnergyScale.signal/muJetEnergyScale.h
+++ uncertainties/jetEnergyScale.signal/muJetEnergyScale.h
@@ -1,18 +1,43 @@
+R__LOAD_LIBRARY(/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/bTagCalhead/BTagCalibrationStandalone_cpp.so)
 #include <vector>
 #include <string>
 #include <iostream>
 #include <TClonesArray.h>
 #include <TLorentzVector.h>
+#include <TGraphAsymmErrors.h>
 #include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/untuplizer.h"
 #include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/isPassZmumu.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/bTagCalhead/BTagCalibrationStandalone.h"
 
-float muJetEnergyScale(string inputFile, int cat, int scale){
+float muJetEnergyScale(string inputFile, string js, int cat){
+
+  // setup calibration and reader
+
+  BTagCalibration calib("csvv1", "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/CSVV1.csv");
+
+  BTagCalibrationReader reader_udsg(BTagEntry::OP_LOOSE, "central");
+  BTagCalibrationReader reader_c(BTagEntry::OP_LOOSE, "central");
+  BTagCalibrationReader reader_b(BTagEntry::OP_LOOSE, "central");
+
+  reader_udsg.load(calib, BTagEntry::FLAV_UDSG, "mujets");
+  reader_c.load(calib, BTagEntry::FLAV_C, "mujets");
+  reader_b.load(calib, BTagEntry::FLAV_B, "mujets");
+
+  // to read b-tag effinciency 
+
+  TFile* f_l = TFile::Open("/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/uncertainties/btagging.zjets/bTagEffroot/mu_udsgflavor_zjetsBtagEff.root");
+  TFile* f_c = TFile::Open("/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/uncertainties/btagging.zjets/bTagEffroot/mu_cflavor_zjetsBtagEff.root");
+  TFile* f_b = TFile::Open("/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/uncertainties/btagging.zjets/bTagEffroot/mu_bflavor_zjetsBtagEff.root");
+  
+  TGraphAsymmErrors* g_l = (TGraphAsymmErrors*)(f_l->Get("mu_udsgflavor"));
+  TGraphAsymmErrors* g_c = (TGraphAsymmErrors*)(f_c->Get("mu_cflavor"));
+  TGraphAsymmErrors* g_b = (TGraphAsymmErrors*)(f_b->Get("mu_bflavor"));
 
   // read the ntuples (in pcncu)
 
   TreeReader data(inputFile.data());
   
-  int passEvent = 0;
+  float passEvent = 0.;
 
   // begin of event loop
 
@@ -20,7 +45,7 @@ float muJetEnergyScale(string inputFile, int cat, int scale){
 
     data.GetEntry(ev);
 
-    TClonesArray*  muP4             = (TClonesArray*) data.GetPtrTObject("muP4");
+    TClonesArray*  muP4              = (TClonesArray*) data.GetPtrTObject("muP4");
     Int_t          FATnJet           = data.GetInt("FATnJet");    
     Int_t*         FATnSubSDJet      = data.GetPtrInt("FATnSubSDJet");
     Float_t*       FATjetPRmassCorr  = data.GetPtrFloat("FATjetPRmassL2L3Corr");
@@ -29,47 +54,37 @@ float muJetEnergyScale(string inputFile, int cat, int scale){
     TClonesArray*  FATjetP4          = (TClonesArray*) data.GetPtrTObject("FATjetP4");
     vector<bool>&  FATjetPassIDLoose = *((vector<bool>*) data.GetPtr("FATjetPassIDLoose"));
     vector<float>* FATsubjetSDCSV    = data.GetPtrVectorFloat("FATsubjetSDCSV", FATnJet);
+    vector<float>* FATsubjetSDPx     = data.GetPtrVectorFloat("FATsubjetSDPx", FATnJet);
+    vector<float>* FATsubjetSDPy     = data.GetPtrVectorFloat("FATsubjetSDPy", FATnJet);
+    vector<float>* FATsubjetSDPz     = data.GetPtrVectorFloat("FATsubjetSDPz", FATnJet);
+    vector<float>* FATsubjetSDE      = data.GetPtrVectorFloat("FATsubjetSDE", FATnJet);
+    vector<int>*   FATsubjetFlavor   = data.GetPtrVectorInt("FATsubjetSDHadronFlavor", FATnJet);
 
     // select good reco level events     
     // select good leptons
-    
+      
     vector<int> goodLepID;
 
     if( !isPassZmumu(data,goodLepID) ) continue;
 
     TLorentzVector* thisLep = (TLorentzVector*)muP4->At(goodLepID[0]);
     TLorentzVector* thatLep = (TLorentzVector*)muP4->At(goodLepID[1]);
-    
+
     // select good FATjet
 
     int goodFATJetID = -1;
     TLorentzVector thisJet(0,0,0,0);
-
+    
     for( int ij = 0; ij < FATnJet; ++ij ){
 
       TLorentzVector* myJet = (TLorentzVector*)FATjetP4->At(ij);
 
-      if( scale != 0 )
-	*myJet *= (scale == 1) ? (1+FATjetCorrUncUp[ij]) : (1-FATjetCorrUncDown[ij]);
-    
+      *myJet *= (js=="central") ? 1 : ( (js=="up") ? (1+FATjetCorrUncUp[ij]) : (1-FATjetCorrUncDown[ij]) );
+
       if( myJet->Pt() < 200 ) continue;
       if( fabs(myJet->Eta()) > 2.4 ) continue;
       if( !FATjetPassIDLoose[ij] ) continue;
       if( myJet->DeltaR(*thisLep) < 0.8 || myJet->DeltaR(*thatLep) < 0.8 ) continue;
-      if( FATjetPRmassCorr[ij] < 105 || FATjetPRmassCorr[ij] > 135 ) continue;
-      
-      int nsubBjet = 0;
-
-      for( int is = 0; is < FATnSubSDJet[ij]; ++is ){
-
-	if( FATsubjetSDCSV[ij][is] > 0.605 ) ++nsubBjet;
-
-      }
- 
-      // b-tag cut
- 
-      if( cat == 1 && nsubBjet != 1 ) continue;
-      if( cat == 2 && nsubBjet != 2 ) continue;
       
       goodFATJetID = ij;
       thisJet = *myJet;
@@ -82,10 +97,66 @@ float muJetEnergyScale(string inputFile, int cat, int scale){
 
     if( (*thisLep+*thatLep+thisJet).M() < 750 ) continue;
 
-    ++passEvent;
+    int nsubBjet = 0;
+    float pMC = 1., pData = 1.;
+
+    for( int is = 0; is < FATnSubSDJet[goodFATJetID]; ++is ){
+            
+      TLorentzVector thisSubJet;
+      
+      thisSubJet.SetPxPyPzE(FATsubjetSDPx[goodFATJetID][is],
+			    FATsubjetSDPy[goodFATJetID][is],
+			    FATsubjetSDPz[goodFATJetID][is],
+			    FATsubjetSDE[goodFATJetID][is]);
+
+      float btagEff, scaleFactor;
+
+      if( FATsubjetFlavor[goodFATJetID][is] != 4 && FATsubjetFlavor[goodFATJetID][is] != 5 ){
+
+	btagEff = g_l->Eval(thisSubJet.Pt());
+	scaleFactor = reader_udsg.eval_auto_bounds("central", BTagEntry::FLAV_UDSG, thisSubJet.Eta(), thisSubJet.Pt());
+
+      }
+
+      else if( FATsubjetFlavor[goodFATJetID][is] == 4 ){
+
+	btagEff = g_c->Eval(thisSubJet.Pt());
+	scaleFactor = reader_c.eval_auto_bounds("central", BTagEntry::FLAV_C, thisSubJet.Eta(), thisSubJet.Pt());
+
+      }
+      else if( FATsubjetFlavor[goodFATJetID][is] == 5 ){
+
+	btagEff = g_b->Eval(thisSubJet.Pt());
+	scaleFactor = reader_b.eval_auto_bounds("central", BTagEntry::FLAV_B, thisSubJet.Eta(), thisSubJet.Pt());
+
+      }
+
+      if( FATsubjetSDCSV[goodFATJetID][is] > 0.605 ){
+
+	pMC   *= btagEff;
+	pData *= scaleFactor * btagEff;
+	++nsubBjet;
+	
+      }
+      
+      else{
+	
+	pMC   *= (1 - btagEff);
+	pData *= (1 - scaleFactor * btagEff);
+	
+      }
+      
+    } // end of subjet loop
+    
+    // b-tag cut
+    
+    if( cat == 1 && nsubBjet != 1 ) continue;
+    if( cat == 2 && nsubBjet != 2 ) continue;
+        
+    passEvent += (pData/pMC);
 
   } // end of event loop
   
-  return (float)passEvent/(float)data.GetEntriesFast();
+  return passEvent/(float)data.GetEntriesFast();
 
 }
