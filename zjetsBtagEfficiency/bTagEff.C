b49918e5d87b268e23d569fef3ded33d744d6691
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-08-18 09:34:20 +0200

    update

--- zjetsBtagEfficiency/bTagEff.C
+++ zjetsBtagEfficiency/bTagEff.C
@@ -1,90 +1,134 @@
-#include <iostream>
-#include <TFile.h>
-#include <TLatex.h>
-#include <TCanvas.h>
-#include <TLegend.h>
-#include <TGraphAsymmErrors.h>
-#include "../readHists.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/untuplizer.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/isPassZee.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/isPassZmumu.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/isPassJet.h"
 
-void bTagEff(string channel, string flavor){
+void bTagEff(string inputFile, string outputFile, string channel){
+  
+  // read the ntuples (in pcncu)
+  
+  TreeReader data(inputFile.data());
 
-  readHist zjets100(Form("%s/Zjets/DYJetsToLL_M-50_HT-100to200_13TeV_%sMCbtagEff.root", channel.data(), channel.data()));
-  readHist zjets200(Form("%s/Zjets/DYJetsToLL_M-50_HT-200to400_13TeV_%sMCbtagEff.root", channel.data(), channel.data()));
-  readHist zjets400(Form("%s/Zjets/DYJetsToLL_M-50_HT-400to600_13TeV_%sMCbtagEff.root", channel.data(), channel.data()));
-  readHist zjets600(Form("%s/Zjets/DYJetsToLL_M-50_HT-600toInf_13TeV_%sMCbtagEff.root", channel.data(), channel.data()));
+  // Declare the histogram
 
-  string ptnoCSVName, ptwtCSVName;
-
-  if( flavor == "udsg" ){
-    ptnoCSVName = "lJetPtnoCSV";
-    ptwtCSVName = "lJetPtwtCSV";
-  }
-
-  else if( flavor == "c" ){
-    ptnoCSVName = "cJetPtnoCSV";
-    ptwtCSVName = "cJetPtwtCSV";
-  }
-
-  else if( flavor == "b" ){
-    ptnoCSVName = "bJetPtnoCSV";
-    ptwtCSVName = "bJetPtwtCSV";
-  }
+  TFile* f = new TFile(inputFile.data());
+  TH1D* h_totalEvents = (TH1D*)f->Get("h_totalEv");
 
   float varBinslight[] = {30,50,70,100,140,200,300,670,1000,2000};
   float varBins[] = {30,50,70,100,140,200,300,670,2000};
-  
+
   int nvarBinslight = sizeof(varBinslight)/sizeof(varBinslight[0])-1;
   int nvarBins = sizeof(varBins)/sizeof(varBins[0])-1;
 
-  TH1F* h_jetPtnoCSV = new TH1F("h_jetPtnoCSV", "", (flavor=="udsg")?nvarBinslight:nvarBins, (flavor=="udsg")?varBinslight:varBins);
-  TH1F* h_jetPtwtCSV = new TH1F("h_jetPtwtCSV", "", (flavor=="udsg")?nvarBinslight:nvarBins, (flavor=="udsg")?varBinslight:varBins);
+  TH1F* h_lJetPtnoCSV = new TH1F("h_lJetPtnoCSV", "lJetPtnoCSV", nvarBinslight, varBinslight);
+  TH1F* h_lJetPtwtCSV = new TH1F("h_lJetPtwtCSV", "lJetPtwtCSV", nvarBinslight, varBinslight);
+  TH1F* h_cJetPtnoCSV = new TH1F("h_cJetPtnoCSV", "cJetPtnoCSV", nvarBins, varBins);
+  TH1F* h_cJetPtwtCSV = new TH1F("h_cJetPtwtCSV", "cJetPtwtCSV", nvarBins, varBins);
+  TH1F* h_bJetPtnoCSV = new TH1F("h_bJetPtnoCSV", "bJetPtnoCSV", nvarBins, varBins);
+  TH1F* h_bJetPtwtCSV = new TH1F("h_bJetPtwtCSV", "bJetPtwtCSV", nvarBins, varBins);
 
-  h_jetPtnoCSV->Add((TH1F*)(zjets100.getHist(ptnoCSVName.data()))->Clone("h_jetPtnoCSV"));
-  h_jetPtnoCSV->Add((TH1F*)(zjets200.getHist(ptnoCSVName.data()))->Clone("h_jetPtnoCSV"));
-  h_jetPtnoCSV->Add((TH1F*)(zjets400.getHist(ptnoCSVName.data()))->Clone("h_jetPtnoCSV"));
-  h_jetPtnoCSV->Add((TH1F*)(zjets600.getHist(ptnoCSVName.data()))->Clone("h_jetPtnoCSV"));
+  // begin of event loop
 
-  h_jetPtwtCSV->Add((TH1F*)(zjets100.getHist(ptwtCSVName.data()))->Clone("h_jetPtwtCSV"));
-  h_jetPtwtCSV->Add((TH1F*)(zjets200.getHist(ptwtCSVName.data()))->Clone("h_jetPtwtCSV"));
-  h_jetPtwtCSV->Add((TH1F*)(zjets400.getHist(ptwtCSVName.data()))->Clone("h_jetPtwtCSV"));
-  h_jetPtwtCSV->Add((TH1F*)(zjets600.getHist(ptwtCSVName.data()))->Clone("h_jetPtwtCSV"));
+  fprintf(stdout, "Total events %lli\n", data.GetEntriesFast());
 
-  TFile f(Form("%s_%sflavor_zjetsBtagEff.root", channel.data(),flavor.data()),"recreate");
+  for( Long64_t ev = data.GetEntriesFast()-1; ev >= 0; --ev ){
 
-  TGraphAsymmErrors *g = new TGraphAsymmErrors();
+    if( (unsigned)ev % 100000 == 0 )
+      fprintf(stdout, "Still left events %lli\n", ev);
 
-  g->BayesDivide(h_jetPtwtCSV, h_jetPtnoCSV, "B");
-  g->SetMarkerStyle(8);
-  g->SetMaximum(1.3);
-  g->GetYaxis()->SetTitle("Efficiency");  
-  g->GetXaxis()->SetTitle("p_{T subjet} [GeV]");
-  g->GetXaxis()->SetLimits(0,2000);
+    data.GetEntry(ev);
 
-  g->Write(Form("%s_%sflavor",channel.data(),flavor.data()));
-  f.Write();
+    Float_t        eventWeight     = data.GetFloat("ev_weight");
+    TClonesArray*  muP4            = (TClonesArray*) data.GetPtrTObject("muP4");
+    TClonesArray*  eleP4           = (TClonesArray*) data.GetPtrTObject("eleP4");
+    TClonesArray*  FATjetP4        = (TClonesArray*) data.GetPtrTObject("FATjetP4");
+    Int_t          FATnJet         = data.GetInt("FATnJet");    
+    Int_t*         FATnSubSDJet    = data.GetPtrInt("FATnSubSDJet");
+    vector<float>* FATsubjetSDCSV  = data.GetPtrVectorFloat("FATsubjetSDCSV", FATnJet);
+    vector<float>* FATsubjetSDPx   = data.GetPtrVectorFloat("FATsubjetSDPx", FATnJet);
+    vector<float>* FATsubjetSDPy   = data.GetPtrVectorFloat("FATsubjetSDPy", FATnJet);
+    vector<float>* FATsubjetSDPz   = data.GetPtrVectorFloat("FATsubjetSDPz", FATnJet);
+    vector<float>* FATsubjetSDE    = data.GetPtrVectorFloat("FATsubjetSDE", FATnJet);
+    vector<int>*   FATsubjetFlavor = data.GetPtrVectorInt("FATsubjetSDHadronFlavor", FATnJet);
 
-  TLegend leg(0.60, 0.70, 0.90, 0.87);
+    // select good reco level events     
+    // select good leptons
+      
+    vector<int> goodLepID;
 
-  leg.SetBorderSize(0);
-  leg.SetFillColor(0);
-  leg.SetFillStyle(0);
-  leg.SetTextSize(0.04);
-  leg.AddEntry(g, Form("%s, %s flavor",channel.data(),flavor.data()), "lp");
+    if( channel == "ele" && !isPassZee(data,goodLepID)   ) continue;
+    if( channel == "mu"  && !isPassZmumu(data,goodLepID) ) continue;
+
+    TLorentzVector* thisLep = (channel=="ele") ? (TLorentzVector*)eleP4->At(goodLepID[0]) : (TLorentzVector*)muP4->At(goodLepID[0]);
+    TLorentzVector* thatLep = (channel=="ele") ? (TLorentzVector*)eleP4->At(goodLepID[1]) : (TLorentzVector*)muP4->At(goodLepID[1]);
+
+    // select good FATjet
+
+    int goodFATJetID = -1;
+
+    if( !isPassJet(data, &goodFATJetID, thisLep, thatLep) ) continue;
+
+    TLorentzVector* thisJet = (TLorentzVector*)FATjetP4->At(goodFATJetID);
+
+    if( (*thisLep+*thatLep+*thisJet).M() < 750 ) continue;
+
+    // b-tag efficiency part
+
+    for( int is = 0; is < FATnSubSDJet[goodFATJetID]; ++is ){
+      
+      TLorentzVector thisSubJet;
+
+      thisSubJet.SetPxPyPzE(FATsubjetSDPx[goodFATJetID][is],
+			    FATsubjetSDPy[goodFATJetID][is],
+			    FATsubjetSDPz[goodFATJetID][is],
+			    FATsubjetSDE[goodFATJetID][is]);
+
+      if( FATsubjetFlavor[goodFATJetID][is] != 4 && FATsubjetFlavor[goodFATJetID][is] != 5 ){
+
+	h_lJetPtnoCSV->Fill(thisSubJet.Pt(),eventWeight);
+	
+	if( FATsubjetSDCSV[goodFATJetID][is] > 0.605 )
+	  h_lJetPtwtCSV->Fill(thisSubJet.Pt(),eventWeight);
+
+      }
+
+      else if( FATsubjetFlavor[goodFATJetID][is] == 4 ){
+
+	h_cJetPtnoCSV->Fill(thisSubJet.Pt(),eventWeight);
+
+	if( FATsubjetSDCSV[goodFATJetID][is] > 0.605 )
+	  h_cJetPtwtCSV->Fill(thisSubJet.Pt(),eventWeight);
+
+      }
+ 
+      else if( FATsubjetFlavor[goodFATJetID][is] == 5 ){
+
+	h_bJetPtnoCSV->Fill(thisSubJet.Pt(),eventWeight);
+
+	if( FATsubjetSDCSV[goodFATJetID][is] > 0.605 )
+	  h_bJetPtwtCSV->Fill(thisSubJet.Pt(),eventWeight);
+
+      } // end of if-else subjet flavor
+ 
+    } // end of subjet loop                           
+
+  } // end of event loop
   
-  TLatex lar;
+  fprintf(stdout, "Processed all events\n");
 
-  lar.SetNDC(kTRUE);
-  lar.SetTextSize(0.04);
-  lar.SetLineWidth(5);
+  TFile* outFile = new TFile(Form("%s_%sMCbtagEff.root",outputFile.data(),channel.data()), "recreate");
 
-  TCanvas c("c", "", 0, 0, 800, 600);
-
-  c.cd();
-  g->Draw("ap");
-  leg.Draw();
-  lar.DrawLatex(0.15, 0.83, "CMS");
-  lar.DrawLatex(0.15, 0.79, "#it{#bf{Simulation}}");
-  lar.DrawLatex(0.62, 0.83, "#bf{Z+jets samples}");
-  c.Print(Form("%s_%sflavor_zjetsBtagEff.pdf", channel.data(), flavor.data()));
+  h_lJetPtnoCSV->Write("lJetPtnoCSV");
+  h_lJetPtwtCSV->Write("lJetPtwtCSV");
+  h_cJetPtnoCSV->Write("cJetPtnoCSV");
+  h_cJetPtwtCSV->Write("cJetPtwtCSV");
+  h_bJetPtnoCSV->Write("bJetPtnoCSV");
+  h_bJetPtwtCSV->Write("bJetPtwtCSV");
+  h_totalEvents->Write("totalEvents");
   
+  outFile->Write();
+
+  delete f;
+  delete outFile;
+
 }
