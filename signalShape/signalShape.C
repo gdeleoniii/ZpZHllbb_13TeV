eb55e6d0cd7c2fc4efff80691b19cf90ad8c33ce
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-08-21 11:50:51 +0200

    update

--- signalShape/signalShape.C
+++ signalShape/signalShape.C
@@ -1,70 +1,125 @@
 R__LOAD_LIBRARY(/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/PDFs/HWWLVJRooPdfs_cxx.so)
 R__LOAD_LIBRARY(/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/PDFs/PdfDiagonalizer_cc.so)
-#include "zpEleShape.h"
-#include "zpMuShape.h"
 using namespace RooFit;
 
-void signalShape(string chan, int cat){
+void signalShape(string channel, string catcut){
+
+  // Suppress all the INFO message
 
   RooMsgService::instance().setGlobalKillBelow(RooFit::FATAL);
   RooMsgService::instance().setSilentMode(true);
 
-  float mzh[10] = {800,1000,1200,1400,1600,1800,2000,2500,3000,3500};
+  int mass[10] = {800,1000,1200,1400,1600,1800,2000,2500,3000,3500};
 
-  RooRealVar mZH("mZH", "m_{ZH} (GeV)", 500, 4000);
-  mZH.setRange("fullRange", 500., 4000.);
-
-  RooPlot* myFrame = mZH.frame();
-  
-  TH1F* f_ = NULL;
-  TLegend* leg = new TLegend(0.65,0.40,0.85,0.75);
-  
   for( int i = 0; i < 10; ++i ){
 
-    RooRealVar m("m", "mean", mzh[i], mzh[i]-50, mzh[i]+50);
+    // Input files
+
+    TChain* treeSignal = new TChain("tree");
+
+    treeSignal->Add(Form("signal/ZprimeToZhToZlephbb_M-%i_13TeV_%sMiniTree.root", mass[i], channel.data()));
+
+    // Define all the variables from the trees
+
+    RooRealVar cat ("cat", "", 0, 2);
+    RooRealVar mZH ("mllbb", "M_{ZH}", 800., 4000., "GeV");
+    RooRealVar evWeight("evweight", "", 0., 1.e3);
+
+    mZH.setRange("fullRange", 800., 4000.);
+
+    RooBinning binsmZH(64, 800, 4000);
+
+    RooArgSet variables(cat, mZH, evWeight);
+
+    TCut catCut = Form("cat==%s", catcut.c_str());
+
+    // Create a dataset from a tree -> to process an unbinned likelihood fitting
+
+    RooDataSet dataSetSignal("dataSetSignal", "dataSetSignal", variables, Cut(catCut), WeightVar(evWeight), Import(*treeSignal));
+
+    RooRealVar nEvents("nEvents", "nEvents", 0., 1.e10);
+
+    nEvents.setVal(dataSetSignal.sumEntries());
+    nEvents.setConstant(true);
+
+    // Fit the signal shape -> using Crystal Ball function
+
+    RooRealVar m("m", "mean", 0., 1.e5);
     RooRealVar s("s", "sigma", 25., 80.);
     RooRealVar a("a", "alpha", 1., 0.1, 3.);
     RooRealVar n("n", "n", 80., 0., 160.);
 
+    m.setVal(mass[i]);
     m.setConstant(true);
 
-    f_ = (chan == "mu") ?
-      zpMuShape(Form("/data7/htong/skim_NCUGlobalTuples/skim_mu_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root",(int)mzh[i]), cat, (int)mzh[i]) :
-      zpEleShape(Form("/data7/htong/skim_NCUGlobalTuples/skim_ele_crab_ZprimeToZhToZlephbb_narrow_M-%d_13TeV-madgraph.root",(int)mzh[i]), cat, (int)mzh[i]);
-
-    RooDataHist h_("h_", "", mZH, Import(*f_));
     RooCBShape model("model", "Cystal Ball Function", mZH, m, s, a, n);
+    RooExtendPdf ext_model("ext_model", "Extended Cystal Ball Function", model, nEvents);
 
-    int color = 100-4*i;
+    ext_model.fitTo(dataSetSignal, SumW2Error(true), Extended(true), Range("fullRange"), Strategy(2), Minimizer("Minuit2"), Save(1));
 
-    mZH.setRange("range", mzh[i]-400, mzh[i]+300);
-    model.fitTo(h_, SumW2Error(true), Range("fullRange"), Strategy(2), Minimizer("Minuit2"), Save(1));
-    h_.plotOn(myFrame,MarkerColor(color),LineColor(color));
-    model.plotOn(myFrame,LineColor(color));
-    leg->AddEntry(myFrame->findObject(myFrame->nameOf(1)), Form("M_{ZH} = %d GeV", (int)mzh[i]), "l");
+    fprintf(stdout, "mass=%i\tmean=%f\tsigma=%f\talpha=%f\tn=%f\n", mass[i], m.getVal(), s.getVal(), a.getVal(), n.getVal());
+
+    // Plot the results to a frame
+
+    RooPlot* signalFrame = mZH.frame();
   
-    fprintf(stdout, "m=%f\ts=%f\ta=%f\tn=%f\n", m.getVal(), s.getVal(), a.getVal(), n.getVal());
-      
+    dataSetSignal.plotOn(signalFrame, Binning(binsmZH));
+    ext_model.plotOn(signalFrame, LineColor(kBlue));
+
+    RooPlot* signalPullFrame = mZH.frame();
+
+    // Output the results
+
+    TLatex lar;
+    lar.SetTextSize(0.03);
+    lar.SetLineWidth(5);
+
+    float up_height = 0.82;
+    float dw_height = (1-up_height)*1.445;
+
+    TCanvas c("c","",0,0,1000,800);
+  
+    c.Divide(1,2);
+
+    TPad* c_up = (TPad*)c.GetListOfPrimitives()->FindObject("c_1");
+    TPad* c_dw = (TPad*)c.GetListOfPrimitives()->FindObject("c_2"); 
+
+    c_up->SetPad(0,1-up_height,1,1);
+    c_dw->SetPad(0,0,1,dw_height);
+    c_dw->SetBottomMargin(0.25);
+    c_up->cd();
+
+    signalFrame->SetTitle("");
+    signalFrame->SetMinimum(0);
+    signalFrame->GetXaxis()->SetTitle("");
+    signalFrame->GetXaxis()->SetLabelOffset(999);
+    signalFrame->Draw();
+
+    lar.DrawLatexNDC(0.12, 0.92, "CMS #it{#bf{Simulation}}");
+    lar.DrawLatexNDC(0.65, 0.92, "L = 2.512 fb^{-1} at #sqrt{s} = 13 TeV");
+    lar.DrawLatexNDC(0.15, 0.86, Form("%s, %s b-tag", channel.data(), catcut.data()));
+    lar.DrawLatexNDC(0.15, 0.82, "Crystal Ball function");
+
+    c_up->RedrawAxis();
+
+    c_dw->cd();
+
+    signalPullFrame->addObject(signalFrame->pullHist(), "P");
+    signalPullFrame->SetTitle("");
+    signalPullFrame->GetYaxis()->SetTitle("Pulls");
+    signalPullFrame->GetYaxis()->SetTitleOffset(0.25);
+    signalPullFrame->GetXaxis()->SetLabelSize(0.125);
+    signalPullFrame->GetXaxis()->SetTitleSize(0.125);
+    signalPullFrame->GetYaxis()->SetLabelSize(0.125);
+    signalPullFrame->GetYaxis()->SetTitleSize(0.125);
+    signalPullFrame->GetYaxis()->SetNdivisions(505);
+    signalPullFrame->SetMinimum(-4);
+    signalPullFrame->SetMaximum(4);
+    signalPullFrame->Draw();
+
+    c.Draw();
+    c.Print(Form("signalShape_%s_%sbtag.pdf%s", channel.data(), catcut.data(), (i==0?"(":(i==9?")":""))));
+
   }
-
-  leg->Draw();
-  myFrame->addObject(leg);
-  myFrame->SetTitle("");
-  myFrame->SetMinimum(1.e-3);
-
-  TLatex lar;
-
-  lar.SetTextSize(0.04);
-  lar.SetLineWidth(5);
-
-  TCanvas c("c", "", 0, 0, 800, 600);
-
-  c.cd();
-  c.SetLogy();
-  myFrame->Draw();
-  lar.DrawLatexNDC(0.20, 0.83, "CMS");
-  lar.DrawLatexNDC(0.20, 0.79, "#it{#bf{Simulation}}");
-  lar.DrawLatexNDC(0.72, 0.79, Form("%s  %i btag", chan.data(), cat));
-  c.Print(Form("signalShape_%s_%ibtag.pdf", chan.data(), cat));
 
 }
---
+++ signalShape/signalTree.C
@@ -0,0 +1,108 @@
+R__LOAD_LIBRARY(/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/bTagCalhead/BTagCalibrationStandalone_cpp.so)
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/readHists.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/untuplizer.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/isPassZee.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/isPassZmumu.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/isPassJet.h"
+#include "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/bTagCalhead/BTagCalibrationStandalone.h"
+
+void signalTree(string inputFile, string outputFile, string channel, int mzh){
+
+  // setup calibration and reader
+
+  BTagCalibration calib("csvv1", "/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/CSVV1.csv");
+
+  BTagCalibrationReader reader_l(BTagEntry::OP_LOOSE, "central");
+  BTagCalibrationReader reader_c(BTagEntry::OP_LOOSE, "central");
+  BTagCalibrationReader reader_b(BTagEntry::OP_LOOSE, "central");
+
+  reader_l.load(calib, BTagEntry::FLAV_UDSG, "comb");
+  reader_c.load(calib, BTagEntry::FLAV_C,    "mujets");
+  reader_b.load(calib, BTagEntry::FLAV_B,    "mujets");
+
+  // to read b-tag effinciency 
+
+  TFile* f_l = TFile::Open(Form("/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/bTagEffroot/%s_udsgflavor_zjetsBtagEff.root", channel.data()));
+  TFile* f_c = TFile::Open(Form("/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/bTagEffroot/%s_cflavor_zjetsBtagEff.root",    channel.data()));
+  TFile* f_b = TFile::Open(Form("/afs/cern.ch/work/h/htong/ZpZHllbb_13TeV/bTagEffroot/%s_bflavor_signalBtagEff.root",   channel.data()));
+  
+  TH1F* h_l = (TH1F*)(f_l->Get(Form("%s_udsgflavor",  channel.data())));
+  TH1F* h_c = (TH1F*)(f_c->Get(Form("%s_cflavor",     channel.data())));
+  TH1F* h_b = (TH1F*)(f_b->Get(Form("%s_bflavor_m%i", channel.data(), mzh)));
+
+  // read the ntuples (in pcncu)
+
+  TreeReader data(inputFile.data());
+  TFile f(inputFile.data());
+
+  // Create a tree to store variables
+
+  TFile* outFile = new TFile(Form("%s_%sMiniTree.root", outputFile.data(), channel.data()), "recreate");
+  TTree* tree = new TTree("tree", "TreeForRooFit");
+
+  Int_t   cat;
+  Float_t mllbb, evweight;
+
+  tree->Branch("cat",      &cat,      "cat/I");
+  tree->Branch("mllbb",    &mllbb,    "mllbb/F");
+  tree->Branch("evweight", &evweight, "evweight/F");
+
+  // Calculate the scale correspond to inputFile
+
+  float scale = 2512.*readHist::crossSection(inputFile.data())/((TH1F*)f.Get("h_totalEv"))->Integral();
+
+  // begin of event loop
+
+  for( Long64_t ev = data.GetEntriesFast()-1; ev >= 0; --ev ){
+
+    data.GetEntry(ev);
+
+    Float_t       eventWeight = data.GetFloat("ev_weight");
+    TClonesArray* muP4        = (TClonesArray*) data.GetPtrTObject("muP4");
+    TClonesArray* eleP4       = (TClonesArray*) data.GetPtrTObject("eleP4");
+    TClonesArray* FATjetP4    = (TClonesArray*) data.GetPtrTObject("FATjetP4");
+
+    // select good reco level events     
+    // select good leptons
+      
+    vector<int> goodLepID;
+
+    if( channel == "ele" && !isPassZee(data,goodLepID)   ) continue;
+    if( channel == "mu"  && !isPassZmumu(data,goodLepID) ) continue;
+
+    TLorentzVector* thisLep = (channel=="ele") ? (TLorentzVector*)eleP4->At(goodLepID[0]) : (TLorentzVector*)muP4->At(goodLepID[0]);
+    TLorentzVector* thatLep = (channel=="ele") ? (TLorentzVector*)eleP4->At(goodLepID[1]) : (TLorentzVector*)muP4->At(goodLepID[1]);
+
+    // select good FATjet
+
+    int goodFATJetID = -1;
+
+    if( !isPassJet(data, &goodFATJetID, thisLep, thatLep) ) continue;
+
+    TLorentzVector* thisJet = (TLorentzVector*)FATjetP4->At(goodFATJetID);
+
+    //if( (*thisLep+*thatLep+*thisJet).M() < 750 ) continue;
+    //if( (*thisLep+*thatLep).DeltaPhi(*thisJet) < 2.5 ) continue;
+    //if( fabs( (*thisLep+*thatLep).Eta() - (*thisJet).Eta() ) > 5 ) continue;
+
+    // b-tag cut
+
+    int nsubBjet = 0;
+
+    float btagWeight = bTagWeight(data, goodFATJetID, &nsubBjet, h_l, h_c, h_b, reader_l, reader_c, reader_b);
+    
+    if     ( nsubBjet == 1 ) cat = 1;
+    else if( nsubBjet == 2 ) cat = 2;      
+    else                     cat = 0;
+
+    mllbb    = (*thisLep+*thatLep+*thisJet).M();
+    evweight = eventWeight * scale * btagWeight;
+
+  } // end of event loop
+
+  tree->Write();  
+  outFile->Write();
+
+  delete outFile;
+
+}
