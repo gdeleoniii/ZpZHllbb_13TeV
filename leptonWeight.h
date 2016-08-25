dc1232da28afdb97b4212936191dbd55edc1283d
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-08-25 16:15:30 +0200

    update

--- leptonWeight.h
+++ leptonWeight.h
@@ -1,6 +1,6 @@
-float leptonWeight(TH2F* h2_lep, TLorentzVector* thisLep=NULL, int lepScale=0){
+float leptonWeight(TH2F* h2_lep, TLorentzVector* thisLep=NULL, bool absEta=true, int lepScale=0){
 
-  float thisLepPt;
+  float thisLepPt, thisLepEta;
 
   float minPt = h2_lep->GetYaxis()->GetBinLowEdge(h2_lep->GetYaxis()->GetFirst());
   float maxPt = h2_lep->GetYaxis()->GetBinUpEdge(h2_lep->GetYaxis()->GetLast());
@@ -14,9 +14,15 @@ float leptonWeight(TH2F* h2_lep, TLorentzVector* thisLep=NULL, int lepScale=0){
   else
     thisLepPt = thisLep->Pt();
 
-  int thisPtBin  = h2_lep->GetYaxis()->FindBin(thisLepPt);
-  int thisEtaBin = h2_lep->GetXaxis()->FindBin(thisLep->Eta());
+  thisLepEta = (absEta) ? fabs(thisLep->Eta()) : thisLep->Eta();
 
-  return h2_lep->GetBinContent(thisEtaBin, thisPtBin) + ( lepScale * h2_lep->GetBinError(thisEtaBin, thisPtBin));
+  int thisPtBin  = h2_lep->GetYaxis()->FindBin(thisLepPt);
+  int thisEtaBin = h2_lep->GetXaxis()->FindBin(thisLepEta);
+
+  float thisWeight = h2_lep->GetBinContent(thisEtaBin, thisPtBin) + ( lepScale * h2_lep->GetBinError(thisEtaBin, thisPtBin));
+
+  if(thisLepPt<50) cout << thisLepPt << "\t" << lepScale << "\t" << thisWeight << endl;
+
+  return thisWeight;
   
 }
