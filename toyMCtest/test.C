af18bb67523854982551781d460f2c5cd552a247
Henry Yee Shian Tong <henryyy040606@hotmail.com>
2016-09-01 08:19:14 +0200

    update

--- toyMCtest/test.C
+++ toyMCtest/test.C
@@ -4,34 +4,28 @@ using namespace RooFit;
 
 void test(){
 
-
   RooMsgService::instance().setGlobalKillBelow(RooFit::ERROR);
 
-
-
   RooRealVar n("n","n",0,1000);
-  n.setVal(1);
+  n.setVal(100);
   n.setConstant(true);
 
   RooBinning binsX(20, 0, 5);
 
-
   RooRealVar x("x", "x", 0, 5);
   RooRealVar lamda("lamda", "lamda", -1, -4, -0.1);
 
-
   lamda.setConstant(true);
-
-
 
   RooExponential model("model", "model", x, lamda);
 
-
-
-  // If everything below can be solve, let's open this extended pdf and study its behaviour
-  // Also can convert RooEffProd to extended
+  // Extended pdf seems only useful when perform fitting. (still need to be proved)
+  // As its behaviour is same as normal pdf ( It doesnt matter what event number you put )
+  // The integral of extpdf is still the same with its original pdf. This makes the plot didnt change (Pls see comments on plot part)
 
   RooExtendPdf extmodel("extmodel", "extmodel", model, n);
+
+  cout << "extmodel.createIntegral(x): " << extmodel.createIntegral(x)->getVal() << endl;
 
   // What is exactly RooProdPdf? when multiply to functions with same observable x, what will be the output? still 2D ?
 
@@ -44,13 +38,16 @@ void test(){
   RooFormulaVar modelB("modelB","2*x+3",x);
   RooEffProd modelY("modelY", "model*modelB", model, modelB);
 
+  // modelY.Print("t");
 
-  modelY.Print("t");
+  RooExtendPdf extmodelY("extmodelY", "extmodelY", modelY, n);
+
+  cout << "extmodelY.createIntegral(x): " << extmodelY.createIntegral(x)->getVal() << endl;
 
   RooAbsReal* intModel = model.createIntegral(x);
 
+  cout << "model.getVal(): " <<  model.getVal() << endl;  // OK so this is actually the return of exp(-2.5) = 0.082085 
   cout << "model.getVal(x): " << model.getVal(x) << endl; // this is equal to model.getVal()/model.createIntegral(x) but what is the physical meaning?
-  cout << "model.getVal(): " <<  model.getVal() << endl;  // OK so this is actually the return of exp(-2.5) = 0.082085
   cout << "model.createIntegral(x): " << intModel->getVal() << endl;
   cout << "model.getVal()/model.createIntegral(x): " << model.getVal()/intModel->getVal() << endl;
 
@@ -69,9 +66,7 @@ void test(){
   // ERROR:InputArguments -- RooArgSet::checkForDup: ERROR argument with name modelY is already in this set
 
   // now the histogram is correct: if you integral exp(-x)*(2x+3) in range [0,0.5] by hand, the answer is 1.36. 
-  // back to the histogram, you can see that the sum of first two bin is 1.36 
-
-
+  // back to the histogram, you can see that the sum of first two bins is 1.36 
 
   TH1* htest = modelY.createHistogram("htest", x, Binning(binsX), ConditionalObservables(x));
 
@@ -88,12 +83,11 @@ void test(){
   // The additional argument RooAbsReal::Raw is used. (RooAbsReal::NumEvent failed. why?)
 
   model.plotOn(fframe, Normalization(model.createIntegral(x)->getVal(), RooAbsReal::Raw), LineColor(kGreen+1));
-  // extmodel.plotOn(fframe, LineColor(kBlue+1));
-  // modelA.plotOn(fframe, ,LineColor(kRed+1));
+  // extmodel.plotOn(fframe, Normalization(extmodel.createIntegral(x)->getVal(), RooAbsReal::Raw), LineColor(kBlue+1));
+  // modelA.plotOn(fframe, LineColor(kRed+1));
   // modelX.plotOn(fframe,LineColor(kOrange+1) );
   // modelB.plotOn(fframe, LineStyle(2), LineColor(kRed+1));
   modelY.plotOn(fframe, Normalization(modelY.createIntegral(x)->getVal(), RooAbsReal::Raw), LineStyle(2),LineColor(kOrange+1) );
-    
 
   //fframe->Print("v");
   fframe->getNormVars()->Print("1");
